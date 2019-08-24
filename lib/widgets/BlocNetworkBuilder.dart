e8f891f0bb61dd00f58a2254ded8b3d242d56f74
Martin <martin.foerster@brickmakers.de>
2019-08-24 17:50:46 +0200

    Class documentation for unintialized property

--- lib/widgets/BlocNetworkBuilder.dart
+++ lib/widgets/BlocNetworkBuilder.dart
@@ -6,6 +6,17 @@ import 'package:flutter_bloc_network/widgets/NetworkStateWidget.dart';
 
 typedef StateValueGetter<S, T> = NetworkState<T> Function(S state);
 
+/// This widget returns a Widget depending on its [state] [NetworkState]
+///
+/// [getValue] is a function where you define the property of [S] that should be represented
+/// [loading] gets returned if the [state] is [NetworkStateLoading]
+/// [error] gets returned if the [state] is [NetworkStateFailed]
+/// [animateSwitch] whether to use [AnimatedSwitcher] or not (default is true)
+/// [uninitialized] gets returned if the [state] is [NetworkStateUninitialized] if this property is not set the loading widget gets returned instead
+/// [builder] this builder gets called if [state] is [NetworkStateSucceeded] and can be used to return a [Widget] based on [T] that gets returned
+/// [initializeEvent] the event [E] that should be dispatch if [getValue] is [NetworkStateUninitialized]
+/// [onSucceeded] this listener gets called if [getValue] changes to [NetworkStateSucceeded] and passes the value [T]
+/// [onFailed] this listener gets called if [getValue] changes to [NetworkStateFailed] and passes the error object
 class BlocNetworkBuilder<B extends Bloc<E, S>, S, E, T>
     extends StatelessWidget {
   final Widget loading;
