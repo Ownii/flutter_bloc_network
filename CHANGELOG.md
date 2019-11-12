d050f1696c5dfe6bb2df0e9088f50121738b60cf
Martin Förster <martin.foerster@brickmakers.de>
2019-11-12 07:32:59 +0100

    Updated dependencies and removed author

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.4 - 12.1.2019
+* Updated flutter_bloc & bloc to 2.0.0
+
 ## 1.0.3 - 22.10.2019
 * Updated flutter_bloc & bloc to 1.0.0
 
--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\Daten\Dokumente\Programmierung\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\Daten\Dokumente\Programmierung\Flutter\flutter_bloc_network\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/martin/Dokumente/Programs/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/martin/Dokumente/Privat/Projekte/Flutter/flutter_bloc_network/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=E:\Daten\Dokumente\Programmierung\Flutter\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/martin/Dokumente/Programs/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
